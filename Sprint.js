7493ea30248f7f0d8c6572b6e88e69d6e4dcf84c
José Miguel González <68620909+DeliriousDELTA@users.noreply.github.com>
2023-04-13 17:10:15 -0600

    Coments

--- Sprint.js
+++ Sprint.js
@@ -1,1 +1,14 @@
+/*  a = ai
+    e = enter
+    i = imes
+    o = ober
+    u = ufat
+
+    solo letras minusculas
+    sin caracteres especiales
+    debe ser ida y vuelta 
+    El usuario debe de escoger si
+*/
+
+
 val Msj = ""
