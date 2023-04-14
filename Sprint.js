2dbf9f0825281ad5da5ea14490c454c0345db6e6
José Miguel González <68620909+DeliriousDELTA@users.noreply.github.com>
2023-04-14 17:10:22 -0600

    Primer prueba encriptador

--- Sprint.js
+++ Sprint.js
@@ -10,4 +10,37 @@
     El usuario debe de escoger si
 */
 
-var Msj = prompt("Ingresa el texto aqui")
+function EncriptarTxt(){
+    var MSG = document.getElementById('texto').value;
+    for(i=0;i==MSG.length;i++){
+        if (MSG[i]=="a"){
+            MSG[i]="ai"
+        }else
+        if (MSG[i]=="e"){
+            MSG[i]="enter"
+        }else
+        if (MSG[i]=="i"){
+            MSG[i]="imes"
+        }else
+        if (MSG[i]=="o"){
+            MSG[i]="ober"
+        }else
+        if (MSG[i]=="u"){
+            MSG[i]="ufat"
+        }
+    }
+    document.getElementById("textOut").innerHTML = MSG
+}
+
+function DesencriptarTxt(){
+    
+			// Obtener el valor del input
+			var textoInput = document.getElementById("texto-input").value;
+
+			// Modificar el texto con HTML
+			var textoModificado = "<b>" + textoInput + "</b>";
+
+			// Mostrar el texto modificado
+			document.getElementById("texto-output").innerHTML = textoModificado;
+
+}
